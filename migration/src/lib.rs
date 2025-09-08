89eec77636f1b5c99e445107c1ea46ee0f670b76
Yuta Takahashi <y.highbridge04@gmail.com>
2025-09-08 12:34:41 +0000

    feat: replace old migration with new providers and models migration

--- migration/src/lib.rs
+++ migration/src/lib.rs
@@ -1,12 +1,14 @@
 pub use sea_orm_migration::prelude::*;
 
-mod m20220101_000001_create_table;
+mod m20250908_000001_create_providers_and_models;
 
 pub struct Migrator;
 
 #[async_trait::async_trait]
 impl MigratorTrait for Migrator {
     fn migrations() -> Vec<Box<dyn MigrationTrait>> {
-        vec![Box::new(m20220101_000001_create_table::Migration)]
+        vec![
+            Box::new(m20250908_000001_create_providers_and_models::Migration),
+        ]
     }
 }
