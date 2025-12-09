1e24f68f791920c21ab0048837cfa66a909c8c44
Ryosuke Asano <s1F102400011@iniad.org>
2025-12-09 09:22:53 +0900

    style: reorder `use` statements in `sysconfig.rs`

--- src/sysconfig.rs
+++ src/sysconfig.rs
@@ -9,8 +9,8 @@ use exec::{core_exec_plugin_package, exec_plugin_package};
 use fetch::{core_fetch_plugin_package, fetch_plugin_package};
 use filesystem::{core_filesystem_plugin_package, filesystem_plugin_package};
 use floorp::{core_floorp_plugin_package, floorp_plugin_package};
-use vscode::{core_vscode_plugin_package, vscode_plugin_package};
 use search::{core_search_plugin_package, search_plugin_package};
+use vscode::{core_vscode_plugin_package, vscode_plugin_package};
 use window::{core_window_plugin_package, window_plugin_package};
 
 /// Builds the static system configuration used during application startup.
